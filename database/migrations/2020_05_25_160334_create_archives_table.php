652c93759ed1527886e619a380a107e7c994e05e
Stephen Fu <stephen.frx@gmail.com>
2020-05-25 16:06:15 +0800

    添加档案表及删除学生表

--- database/migrations/2020_05_25_160334_create_archives_table.php
+++ database/migrations/2020_05_25_160334_create_archives_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+class CreateArchivesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('archives', function (Blueprint $table) {
+            $table->string('id', 20)->comment('档案编号');
             $table->string('sid', 12)->comment('学号');
             $table->string('name', 50)->comment('姓名');
             $table->string('department_id', 2)->comment('学院');
             $table->string('major_id', 7)->comment('专业');
             $table->string('grade', 4)->comment('年级');
             $table->timestamps();
+
+            $table->primary('id');
         });
     }
 
@@ -30,6 +33,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('archives');
     }
 }
