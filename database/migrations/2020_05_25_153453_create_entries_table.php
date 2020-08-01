14df0191ee0981c3dc16bbd6f33d2181f9101af2
Stephen Fu <stephen.frx@gmail.com>
2020-08-02 00:33:13 +0800

    添加学生表

--- database/migrations/2020_05_25_153453_create_entries_table.php
+++ database/migrations/2020_05_25_153453_create_entries_table.php
@@ -17,6 +17,7 @@ class CreateEntriesTable extends Migration
             $table->id();
             $table->string('name', 64)->unique()->comment('名称');
             $table->boolean('is_enable')->default(true)->comment('是否启用，0-未启用，1-启用');
+            $table->unsignedBigInteger('level_id')->default(1)->comment('培养层次ID');
             $table->text('description')->nullable()->comment('描述');
             $table->integer('order')->default(0)->comment('排序');
             $table->timestamps();
---
+++ database/migrations/2020_08_01_235946_create_students_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->string('id', 12)->comment('学号');
+            $table->string('name', 60)->comment('姓名');
+            $table->string('card_type', 10)->comment('证件类型');
+            $table->string('card_id', 18)->comment('证件号码');
+            $table->string('gender', 2)->comment('性别');
+            $table->string('nation', 20)->comment('民族');
+            $table->string('department_id', 2)->comment('学院代码');
+            $table->string('department', 60)->comment('学院');
+            $table->string('major_id', 7)->comment('专业代码');
+            $table->string('major', 60)->comment('专业');
+            $table->string('grade', 4)->comment('年级');
+            $table->string('duration', 4)->comment('学制');
+            $table->string('status', 20)->comment('学籍状态');
+            $table->string('level', 20)->comment('培养层次');
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
