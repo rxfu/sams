e42ef8ef4cacac1f49eda09e61103bba4e137cbf
Stephen Fu <stephen.frx@gmail.com>
2020-08-16 19:17:37 +0800

    修改档案材料移交表

--- app/Exports/ArchiveExport.php
+++ app/Exports/ArchiveExport.php
@@ -4,9 +4,14 @@ namespace App\Exports;
 
 use App\Models\Entry;
 use App\Models\Student;
+use Illuminate\Contracts\View\View;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\DefaultValueBinder;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
 
-class ArchiveExport implements FromView
+class ArchiveExport extends DefaultValueBinder implements FromView, WithCustomValueBinder
 {
     /**
      * @return View
@@ -20,5 +25,24 @@ class ArchiveExport implements FromView
             ->get();
 
         return view('exports.archive', compact('entries', 'students'));
+    }
+
+    /**
+     * Bind value to a cell.
+     *
+     * @param Cell $cell Cell to bind value to
+     * @param mixed $value Value to bind in cell
+     *
+     * @return bool
+     */
+    public function bindValue(Cell $cell, $value)
+    {
+        if (is_numeric($value)) {
+            $cell->setValueExplicit($value, DataType::TYPE_STRING);
+
+            return true;
+        }
+
+        return parent::bindValue($cell, $value);
     }
 }
--- resources/views/exports/archive.blade.php
+++ resources/views/exports/archive.blade.php
@@ -1,23 +1,29 @@
+
+<h1>本科生新生档案材料移交登记表</h1>
+<p>人数：{{ $students->count() }} 人</p>
 <table>
-    <caption>本科生新生档案材料移交登记表</caption>
     <thead>
-        <th>学号</th>
-        <th>姓名</th>
-        @foreach ($entries as $entry)
-            <th>{{ $entry->name }}</th>
-        @endforeach
-        <th>备注</th>
+        <tr>
+            <th>学号</th>
+            <th>姓名</th>
+            @foreach ($entries as $entry)
+                <th>{{ $entry->name }}</th>
+            @endforeach
+            <th>备注</th>
+        </tr>
     </thead>
     <tbody>
         @foreach ($students as $student)
-            @foreach ($student->archives as $archive)
+            <tr>
                 <td>{{ $student->id }}</td>
                 <td>{{ $student->name }}</td>
-                @foreach ($archive->entries as $item)
-                    {{ $item->quantity }}
+                @foreach ($student->archives as $archive)
+                    @foreach ($archive->entries as $item)
+                        {{ $item->quantity }}
+                    @endforeach
+                    <td>{{ $archive->remark }}</td>
                 @endforeach
-                <td>{{ $archive->remark }}</td>
-            @endforeach
+            </tr>
         @endforeach
     </tbody>
 </table>
